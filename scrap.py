ef9c30096fc445aaa198a3e6dafaf64fabb3d0f0
ROHIT RAJPUT <114306698+ROHIT-XD-KING@users.noreply.github.com>
2023-01-17 20:18:42 +0530

    Delete scrap.py

--- scrap.py
+++
@@ -1,7 +0,0 @@
-import os, platform, time
-os.system('git pull')
-os.system('clear')
-print('follow my github and star my repo');time.sleep(3)
-os.system('xdg-open https://github.com/Ahmed-XD/scrap')
-os.system('clear')
-import xnx
